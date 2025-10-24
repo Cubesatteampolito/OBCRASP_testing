00c4e5a778616537be87c515c87b286b82b122cc
Marco Grisolia <116038537+MarcoGrisolia@users.noreply.github.com>
2025-10-24 19:58:29 +0200

    CS and RST and BUS fix

--- COMMSYS/SX1276_TX.py
+++ COMMSYS/SX1276_TX.py
@@ -5,10 +5,10 @@ import time
 import sys
 
 # --- SPI and GPIO Setup ---
-SPI_BUS = 0
-SPI_DEVICE = 1
+SPI_BUS = 1
+SPI_DEVICE = 2
 GPIO_RESET = 17    # GPIO pin for Reset
-GPIO_CS = 8        # GPIO pin for Chip Select (connected to NSS)
+GPIO_CS = 16        # GPIO pin for Chip Select (connected to NSS)
 GPIO_DIO0 = 24     # GPIO pin for DIO0 (interrupt, mapped to TxDone)
 
 # --- Register Definitions for FSK/OOK Mode ---
