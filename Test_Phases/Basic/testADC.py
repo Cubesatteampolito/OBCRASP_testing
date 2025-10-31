89c4f3c1f2c2c7a37d13e58703b854fcb217b889
GiampaoloCabras <s307902@studenti.polito.it>
2025-10-31 13:15:39 +0100

    ignore

--- Test_Phases/Basic/testADC.py
+++ Test_Phases/Basic/testADC.py
@@ -25,5 +25,16 @@ for ch in range(8):
 #printing conversion results from the list of read messages
 print("Conversion results:")
 for ch in range(len(convres)):
-	print("Channel {0}: {1}".format(ch,int(convres[ch][0]*256+convres[ch][1])*2.5/4095))
+	convres[ch]=convres[ch][0]*256+convres[ch][1]
+	convres[ch] = convres[ch]*2.5/4095
+	
+
+convres[0]= convres[0] * 2 #V5
+print("V5 VALUE: {0}".format(convres[0]))
+convres[1]= convres[1] / 0.3 #I5
+print("I5 VALUE: {0}".format(convres[1]))
+convres[2] = convres[2]*5.255319    #VB
+print("VB VALUE: {0}".format(convres[2]))
+convres[3] = convres[3]/0.30060  #IB
+print("IB VALUE: {0}".format(convres[3]))
 
